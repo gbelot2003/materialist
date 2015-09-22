e9bdf1a1572eaf4d71087426fa1c4742189b0b8a
gbelot2003 <gbelot2003@hotmail.com>
2015-09-21 18:29:36 -0600

    Changing the block title side

--- templates/block/block.tpl.php
+++ templates/block/block.tpl.php
@@ -48,7 +48,7 @@
 
 	<?php print render($title_prefix); ?>
 	<?php if ($block->subject): ?>
-		<h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
+		<h4 <?php print $title_attributes; ?>><?php print $block->subject ?></h4>
 	<?php endif;?>
 	<?php print render($title_suffix); ?>
 
